e11bc7c3d0b2156c48097be34d76dc08fcd3061d
Shantanu Patel <shantu.patel@gmail.com>
2024-04-02 14:03:19 +0530

    User model updated with Entity annotation for mapping to database table.

--- src/main/java/com/social/models/User.java
+++ src/main/java/com/social/models/User.java
@@ -1,7 +1,15 @@
 package com.social.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String firstName;
 	private String lastName;
