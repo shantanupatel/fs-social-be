90609122261f91d12f9d48e8fae1f543927ad8db
Shantanu Patel <shantu.patel@gmail.com>
2024-04-02 13:13:22 +0530

    UserController updated with methods for adding new User, updating existing user and deleting existing user.

--- src/main/java/com/social/controller/UserController.java
+++ src/main/java/com/social/controller/UserController.java
@@ -3,15 +3,22 @@ package com.social.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.social.models.User;
 
 @RestController
+@RequestMapping(value = "/users")
 public class UserController {
 
-	@GetMapping("/users")
+	@GetMapping
 	public List<User> getUsers() {
 
 		List<User> users = new ArrayList<>();
@@ -23,5 +30,53 @@ public class UserController {
 		users.add(user2);
 
 		return users;
+	}
+
+	@GetMapping("/{userId}")
+	public User getUsersById(@PathVariable("userId") int id) {
+		User user1 = new User(1, "John", "Doe", "john.doe@gmail.com", "12345");
+
+		return user1;
+	}
+
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		User newUser = new User();
+
+		newUser.setId(user.getId());
+		newUser.setFirstName(user.getFirstName());
+		newUser.setLastName(user.getLastName());
+		newUser.setEmail(user.getEmail());
+		newUser.setPassword(user.getPassword());
+
+		return newUser;
+	}
+
+	@PutMapping
+	public User updateUser(@RequestBody User user) {
+		User user1 = new User(1, "John", "Doe", "john.doe@gmail.com", "12345");
+
+		if (user.getFirstName() != null) {
+			user1.setFirstName(user.getFirstName());
+		}
+
+		if (user.getLastName() != null) {
+			user1.setLastName(user.getLastName());
+		}
+
+		if (user.getEmail() != null) {
+			user1.setEmail(user.getEmail());
+		}
+
+		if (user.getPassword() != null) {
+			user1.setPassword(user.getPassword());
+		}
+
+		return user1;
+	}
+
+	@DeleteMapping("/{userId}")
+	public String deleteUser(@PathVariable int userId) {
+		return "User deleted successfully with id: " + userId;
 	}
 }
