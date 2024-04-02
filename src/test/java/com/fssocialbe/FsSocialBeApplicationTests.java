7fdd447b5c0815d2e962441b2713fab8d6dcb96a
Shantanu Patel <shantu.patel@gmail.com>
2024-04-02 10:43:53 +0530

    Spring Boot project created for FullStack Social application

---
+++ src/test/java/com/fssocialbe/FsSocialBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fssocialbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsSocialBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
