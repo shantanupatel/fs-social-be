0958559c4e53ee04f1e664cfb5bd3666420e115a
Shantanu Patel <shantu.patel@gmail.com>
2024-04-09 13:34:04 +0530

    PostService interface file updated with required methods for Post.

--- src/main/java/com/social/service/PostService.java
+++ src/main/java/com/social/service/PostService.java
@@ -1,9 +1,23 @@
 package com.social.service;
 
+import java.util.List;
+
 import com.social.models.Post;
 
 public interface PostService {
 
 	Post createNewPost(Post post, int userId) throws Exception;
+
+	String deletePost(int postId, int userId);
+
+	List<Post> findPostByUserId(int userId);
+
+	Post findPostById(int postId);
+
+	List<Post> findAllPosts();
+
+	Post savedPost(int postId, int userId);
+
+	Post likePost(int postId, int userId);
 
 }
