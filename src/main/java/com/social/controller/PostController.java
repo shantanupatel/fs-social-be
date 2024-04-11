7983480e81f5b123fff73a043eacb341249e8f2d
Shantanu Patel <shantu.patel@gmail.com>
2024-04-11 10:04:02 +0530

    PostController class file updated with end-point for creating a new post.

--- src/main/java/com/social/controller/PostController.java
+++ src/main/java/com/social/controller/PostController.java
@@ -1,24 +1,37 @@
 package com.social.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.social.models.Post;
+import com.social.service.PostService;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostController {
 
-	// @GetMapping()
-	// public String getPost() {
-	// return "Hello from Post";
-	// }
+	PostService postService;
 
-	@PostMapping
-	public Post createPost(@RequestBody Post post) {
-		return null;
+	public PostController() {
+	}
+
+	@Autowired
+	public PostController(PostService postService) {
+		this.postService = postService;
+	}
+
+	@PostMapping("/user/{userId}")
+	public ResponseEntity<Post> createPost(@RequestBody Post post, @PathVariable int userId) throws Exception {
+
+		Post createdPost = postService.createNewPost(post, userId);
+
+		return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
 	}
 
 	// deletePost
