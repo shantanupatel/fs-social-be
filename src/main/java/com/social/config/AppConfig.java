3452b98f5219d72c8a749f07fca93742460fab88
Shantanu Patel <shantu.patel@gmail.com>
2024-05-20 16:10:28 +0530

    AppConfig class created with basic configuration required for authentication.

---
+++ src/main/java/com/social/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.social.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(
+						Authorize -> Authorize.requestMatchers("/users/public/***").permitAll().anyRequest().authenticated())
+				.httpBasic().and()
+				.csrf(csrf -> csrf.disable());
+
+		return http.build();
+	}
+}
